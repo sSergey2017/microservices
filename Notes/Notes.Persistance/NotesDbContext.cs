e1e6c7ab3674eeac02d148d203dfee15dbaf8843
sergeysud <sergeysud@wizardsdev.com>
2022-02-08 23:00:22 +0200

    add context DB

---
+++ Notes/Notes.Persistance/NotesDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Interfaces;
+using Notes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.Persistance
+{
+    public class NotesDbContext : DbContext, INotesDbContext
+    {
+        public DbSet<Note> Notes { get ; set ; }
+        public NotesDbContext(DbContextOptions<NotesDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder)
+    }
+}
