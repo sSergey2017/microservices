bd2fba665d6c0eeba04cde163f27fc8cc4b0cd34
sergeysud <sergeysud@wizardsdev.com>
2022-02-14 22:02:44 +0200

    add custom middleware

--- Notes/Notes.Application/DependencyInjection.cs
+++ Notes/Notes.Application/DependencyInjection.cs
@@ -2,12 +2,7 @@
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Notes.Application.Common.Behaviors;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Notes.Application
 {
