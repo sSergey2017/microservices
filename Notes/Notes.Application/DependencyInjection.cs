cbffc7672dc2c01940ff3aefec56156e0bb4ff25
sergeysud <sergeysud@wizardsdev.com>
2022-02-14 21:32:20 +0200

    add fluent validations

--- Notes/Notes.Application/DependencyInjection.cs
+++ Notes/Notes.Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Common.Behaviors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +17,10 @@ namespace Notes.Application
             this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
-            //services
-            //    .AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
-            //services.AddTransient(typeof(IPipelineBehavior<,>),
-            //    typeof(ValidationBehavior<,>));
+            services
+                .AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+            services.AddTransient(typeof(IPipelineBehavior<,>),
+                typeof(ValidationBehavior<,>));
             //services.AddTransient(typeof(IPipelineBehavior<,>),
             //    typeof(LoggingBehavior<,>));
             return services;
